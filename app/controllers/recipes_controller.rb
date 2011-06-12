3cf51256295b2784ca4e2bf4dcb77d9a56c57d08
Greg Fairbrother <greg@intrepica.com.au>
2011-06-12 16:02:52 +1000

    jasonified the rails controller and model

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,83 +1,58 @@
 class RecipesController < ApplicationController
-  # GET /recipes
-  # GET /recipes.xml
+
+  respond_to :json
+
+  before_filter :strip_non_mass_assignment_vars
+
   def index
     @recipes = Recipe.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @recipes }
-    end
+    respond_with @recipes
   end
 
-  # GET /recipes/1
-  # GET /recipes/1.xml
   def show
     @recipe = Recipe.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @recipe }
-    end
+    respond_with @recipe
   end
 
-  # GET /recipes/new
-  # GET /recipes/new.xml
   def new
     @recipe = Recipe.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @recipe }
-    end
+    respond_with @recipe
   end
 
-  # GET /recipes/1/edit
   def edit
     @recipe = Recipe.find(params[:id])
   end
 
-  # POST /recipes
-  # POST /recipes.xml
   def create
-    @recipe = Recipe.new(params[:recipe])
+    @recipe = Recipe.new(params)
 
-    respond_to do |format|
-      if @recipe.save
-        format.html { redirect_to(@recipe, :notice => 'Recipe was successfully created.') }
-        format.xml  { render :xml => @recipe, :status => :created, :location => @recipe }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @recipe.errors, :status => :unprocessable_entity }
-      end
+    if @recipe.save
+      respond_with @recipe
+    else
+      render :json => @recipe.errors, :status => :bad_request
     end
   end
 
-  # PUT /recipes/1
-  # PUT /recipes/1.xml
   def update
     @recipe = Recipe.find(params[:id])
 
-    respond_to do |format|
-      if @recipe.update_attributes(params[:recipe])
-        format.html { redirect_to(@recipe, :notice => 'Recipe was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @recipe.errors, :status => :unprocessable_entity }
-      end
+    if @recipe.update_attributes(params)
+      respond_with @recipe
+    else
+      render :json => @recipe.errors, :status => :bad_request
     end
   end
 
-  # DELETE /recipes/1
-  # DELETE /recipes/1.xml
   def destroy
     @recipe = Recipe.find(params[:id])
     @recipe.destroy
+    respond_with @recipe
+  end
 
-    respond_to do |format|
-      format.html { redirect_to(recipes_url) }
-      format.xml  { head :ok }
-    end
+  protected
+  def strip_non_mass_assignment_vars
+    params.delete(:action)
+    params.delete(:authenticity_token)
+    params.delete(:controller)
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ActiveRecord::Base
+  self.include_root_in_json = false
+
+  validates_presence_of :name, :description
 end
